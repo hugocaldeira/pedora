66e00c067fceaccd0dd02e0080f6f8bfbd62d085
Hugo Caldeira <hugoduquecaldeira@gmail.com>
2022-12-16 22:12:55 +0000

    whatever

--- src/pages/Weather.tsx
+++ src/pages/Weather.tsx
@@ -1,11 +1,33 @@
 import useWeatherService from '../hooks/useWeatherService';
 
+import CanvasJSReact from '../assets/canvasjs.react';
+
 const Weather = () => {
+  const CanvasJS = CanvasJSReact.CanvasJS;
+  const CanvasJSChart = CanvasJSReact.CanvasJSChart;
   const { data, loading, error } = useWeatherService('Porto');
+
+  const options = {
+    title: {
+      text: 'Basic Column Chart in React',
+    },
+    data: [
+      {
+        type: 'column',
+        dataPoints: [
+          { label: 'Apple', y: 10 },
+          { label: 'Orange', y: 15 },
+          { label: 'Banana', y: 25 },
+          { label: 'Mango', y: 30 },
+          { label: 'Grape', y: 28 },
+        ],
+      },
+    ],
+  };
 
   console.log('weather data', data);
 
-  return <h1>weather page</h1>;
+  return <CanvasJSChart options={options} />;
 };
 
 export default Weather;
