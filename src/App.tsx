0035435605f3e73b8f59b164d1cf6b1bbb9814e8
Hugo Caldeira <hugoduquecaldeira@gmail.com>
2023-03-13 22:49:17 +0000

    feat:UI improvemnts

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import { ConfigProvider, Layout, theme } from "antd";
-import News from "./pages/News";
-import Title from "antd/es/typography/Title";
-import MainMenu from "./components/MainMenu";
-import { Route } from "react-router-dom";
-import Weather from "./pages/Weather";
-import SecretFriend from "./pages/SecretFriend";
-import {
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ConfigProvider, Layout, theme, Image } from 'antd';
+import logo from './assets/images/pedora.png';
+import News from './pages/News';
+import Title from 'antd/es/typography/Title';
+import MainMenu from './components/MainMenu';
+import { Route } from 'react-router-dom';
+import Weather from './pages/Weather';
+import SecretFriend from './pages/SecretFriend';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import About from './pages/About.';
 
 const queryClient = new QueryClient();
 
@@ -23,20 +22,25 @@ function App() {
           algorithm: theme.defaultAlgorithm,
         }}
       >
-        <Layout style={{ minHeight: "100vh" }}>
-          <Sider style={{ backgroundColor: "#ffffff" }}>
-            <Title style={{ textAlign: "center" }}>PEDORA</Title>
+        <Layout style={{ minHeight: '100vh' }}>
+          <Sider style={{ backgroundColor: '#ffffff' }}>
+            <Title>
+              <Image src={logo} preview={false} />
+            </Title>
             <MainMenu />
           </Sider>
-          <Content style={{ backgroundColor: "#fafafa", padding: "30px" }}>
-            <Route path="/news">
+          <Content style={{ backgroundColor: '#fafafa', padding: '30px' }}>
+            <Route path='/news'>
               <News />
             </Route>
-            <Route path="/weather">
+            <Route path='/weather'>
               <Weather />
             </Route>
-            <Route path="/secret-friend">
+            <Route path='/secret-friend'>
               <SecretFriend />
+            </Route>
+            <Route path='/about'>
+              <About />
             </Route>
           </Content>
         </Layout>
